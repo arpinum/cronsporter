5371e485cad4345b836520271bec0822ba6371a4
Jean-Baptiste Dusseaut <bodysplash@gmail.com>
2016-03-04 17:52:06 +0100

    seeeed

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,8 +4,8 @@ PATTERN=${PATTERN:-* * * * *}
 SCRIPT_PATH=${SCRIPT_PATH:-"$DIR/work/task.sh"}
 echo "Scheduling $SCRIPT_PATH with pattern $PATTERN"
 cp "$DIR/job.sh" /etc/cron.d/job
-sed -i '' "s/{{PATTERN}}/${PATTERN}/g" /etc/cron.d/job
-sed -i '' "s/{{SCRIPT_PATH}}/${SCRIPT_PATH}/g" /etc/cron.d/job
+sed -i "s/{{PATTERN}}/${PATTERN}/g" /etc/cron.d/job
+sed -i "s|{{SCRIPT_PATH}}|${SCRIPT_PATH}|g" /etc/cron.d/job
 
 chmod 0644 /etc/cron.d/job
 chmod +x "$SCRIPT_PATH"
